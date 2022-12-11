5b7cf578bfca49c8c892a692a04f1157ec6812eb
Maulana Arya Yoga J <116637724+maulanaryoga@users.noreply.github.com>
2022-12-11 12:30:29 +0700

    Add files via upload

---
+++ src/styles/color.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    homeBackgroud: '#d3d3d3',
+    noteBackground: '#fff',
+    notes:'#fff',
+    header: '#7cfc00',
+    headerButtons:'#000',
+    addButton:'#7cfc00',
+    loading:'#0000ff',
+    searchBar:'#f5fffa'
+}
+
+export default COLORS;
